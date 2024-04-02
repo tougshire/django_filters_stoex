33a26dff02b183a42e7340ff5c1fd7ae1063dc1c
tougshire <tougshire@tougshire.com>
2024-04-02 10:10:25 -0400

    Change __init__ args

--- filterset.py
+++ filterset.py
@@ -3,8 +3,8 @@ from django_filters import FilterSet
 
 class StoexFilterSet(FilterSet):
 
-    def __init__(self, data=None, queryset=None, *, request=None, prefix=None):
-        super().__init__(data, queryset)
+    def __init__(self, data=None, *args, **kwargs):
+        super().__init__(data, *args, **kwargs)
         if not self.is_bound:
             for filter in self.filters.values():
                 if "initial" in filter.extra:
